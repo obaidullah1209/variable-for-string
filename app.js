4af3c133f6fe71722848189cde9ab3329b956894
obaidullah1209 <mobaidullah554@gmail.com>
2024-02-29 19:22:38 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// question1
+var username;
+// question2
+var myname = "Muhammad obaidullah";
+alert(myname);
+// question3
+var message = "HELLO WORLD";
+alert(message);
+// question4
+var naam = "Jhone Doe";
+alert(naam);
+var year = "15 year old";
+alert(year);
+var sec = " Web and App";
+alert(sec);
+// questin6
+var pizza =" PIZZA \n PIZZ \n PIZ \n PI \n P"
+alert(pizza)
+// question7
+var email = " My Email Adress Is someone@1234"
+alert(email)
+// question8
+document.write("Yah! I can write HTML content through JS")
+
+
